1cb6b5ea49ab510fab3313f413dffccd85630da5
CJ Kralowetz <ckralowetz@vsi-corp.com>
2014-05-07 09:21:04 -0400

    Regenerated for 1.10

--- cpp/FMDemodulator_base.cpp
+++ cpp/FMDemodulator_base.cpp
@@ -1,3 +1,23 @@
+/*
+ * This file is protected by Copyright. Please refer to the COPYRIGHT file
+ * distributed with this source distribution.
+
+ * This file is part of FMDemodulator.
+
+ * FMDemodulator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+
+ * FMDemodulator is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
 #include "FMDemodulator_base.h"
 
 /*******************************************************************************************
@@ -12,85 +32,40 @@
 
 FMDemodulator_base::FMDemodulator_base(const char *uuid, const char *label) :
     Resource_impl(uuid, label),
-    serviceThread(0)
+    ThreadedComponent()
 {
-    construct();
+    loadProperties();
+
+    dataFloat_in = new bulkio::InFloatPort("dataFloat_in");
+    addPort("dataFloat_in", dataFloat_in);
+    dataFloat_out = new bulkio::OutFloatPort("dataFloat_out");
+    addPort("dataFloat_out", dataFloat_out);
 }
 
-void FMDemodulator_base::construct()
+FMDemodulator_base::~FMDemodulator_base()
 {
-    Resource_impl::_started = false;
-    loadProperties();
-    serviceThread = 0;
-    
-    PortableServer::ObjectId_var oid;
-    dataFloat_in = new bulkio::InFloatPort("dataFloat_in");
-    oid = ossie::corba::RootPOA()->activate_object(dataFloat_in);
-    dataFloat_out = new bulkio::OutFloatPort("dataFloat_out");
-    oid = ossie::corba::RootPOA()->activate_object(dataFloat_out);
-
-    registerInPort(dataFloat_in);
-    registerOutPort(dataFloat_out, dataFloat_out->_this());
+    delete dataFloat_in;
+    dataFloat_in = 0;
+    delete dataFloat_out;
+    dataFloat_out = 0;
 }
 
 /*******************************************************************************************
     Framework-level functions
     These functions are generally called by the framework to perform housekeeping.
 *******************************************************************************************/
-void FMDemodulator_base::initialize() throw (CF::LifeCycle::InitializeError, CORBA::SystemException)
-{
-}
-
 void FMDemodulator_base::start() throw (CORBA::SystemException, CF::Resource::StartError)
 {
-    boost::mutex::scoped_lock lock(serviceThreadLock);
-    if (serviceThread == 0) {
-        dataFloat_in->unblock();
-        serviceThread = new ProcessThread<FMDemodulator_base>(this, 0.1);
-        serviceThread->start();
-    }
-    
-    if (!Resource_impl::started()) {
-    	Resource_impl::start();
-    }
+    Resource_impl::start();
+    ThreadedComponent::startThread();
 }
 
 void FMDemodulator_base::stop() throw (CORBA::SystemException, CF::Resource::StopError)
 {
-    boost::mutex::scoped_lock lock(serviceThreadLock);
-    // release the child thread (if it exists)
-    if (serviceThread != 0) {
-        dataFloat_in->block();
-        if (!serviceThread->release(2)) {
-            throw CF::Resource::StopError(CF::CF_NOTSET, "Processing thread did not die");
-        }
-        serviceThread = 0;
+    Resource_impl::stop();
+    if (!ThreadedComponent::stopThread()) {
+        throw CF::Resource::StopError(CF::CF_NOTSET, "Processing thread did not die");
     }
-    
-    if (Resource_impl::started()) {
-    	Resource_impl::stop();
-    }
-}
-
-CORBA::Object_ptr FMDemodulator_base::getPort(const char* _id) throw (CORBA::SystemException, CF::PortSupplier::UnknownPort)
-{
-
-    std::map<std::string, Port_Provides_base_impl *>::iterator p_in = inPorts.find(std::string(_id));
-    if (p_in != inPorts.end()) {
-        if (!strcmp(_id,"dataFloat_in")) {
-            bulkio::InFloatPort *ptr = dynamic_cast<bulkio::InFloatPort *>(p_in->second);
-            if (ptr) {
-                return ptr->_this();
-            }
-        }
-    }
-
-    std::map<std::string, CF::Port_var>::iterator p_out = outPorts_var.find(std::string(_id));
-    if (p_out != outPorts_var.end()) {
-        return CF::Port::_duplicate(p_out->second);
-    }
-
-    throw (CF::PortSupplier::UnknownPort());
 }
 
 void FMDemodulator_base::releaseObject() throw (CORBA::SystemException, CF::LifeCycle::ReleaseError)
@@ -101,13 +76,6 @@ void FMDemodulator_base::releaseObject() throw (CORBA::SystemException, CF::Life
     } catch (CF::Resource::StopError& ex) {
         // TODO - this should probably be logged instead of ignored
     }
-
-    // deactivate ports
-    releaseInPorts();
-    releaseOutPorts();
-
-    delete(dataFloat_in);
-    delete(dataFloat_out);
 
     Resource_impl::releaseObject();
 }
@@ -151,3 +119,5 @@ void FMDemodulator_base::loadProperties()
                 "configure");
 
 }
+
+
--- tests/test_FMDemodulator.py
+++ tests/test_FMDemodulator.py
@@ -3,84 +3,168 @@
 # This file is protected by Copyright. Please refer to the COPYRIGHT file
 # distributed with this source distribution.
 #
-# This file is part of FMDemodulator.
+# This file is part of REDHAWK.
 #
-# FMDemodulator is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
+# REDHAWK is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# FMDemodulator is distributed in the hope that it will be useful, but WITHOUT
+# REDHAWK is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
 # for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see http://www.gnu.org/licenses/.Boston, MA 02110-1301, USA.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 
-import unittest
-import ossie.utils.testing
-import os
-from omniORB import any
 
-class ComponentTests(ossie.utils.testing.ScaComponentTestCase):
-    """Test for all component implementations in FMDemodulator"""
+import numpy as np
+import scipy.signal as sp
+from ossie.utils import sb
+import time
+import scipy
+import matplotlib.pyplot as plt
 
-    def testScaBasicBehavior(self):
-        #######################################################################
-        # Launch the component with the default execparams
-        execparams = self.getPropertySet(kinds=("execparam",), modes=("readwrite", "writeonly"), includeNil=False)
-        execparams = dict([(x.id, any.from_any(x.value)) for x in execparams])
-        self.launch(execparams)
-        
-        #######################################################################
-        # Verify the basic state of the component
-        self.assertNotEqual(self.comp, None)
-        self.assertEqual(self.comp.ref._non_existent(), False)
-        self.assertEqual(self.comp.ref._is_a("IDL:CF/Resource:1.0"), True)
-        
-        #######################################################################
-        # Validate that query returns all expected parameters
-        # Query of '[]' should return the following set of properties
-        expectedProps = []
-        expectedProps.extend(self.getPropertySet(kinds=("configure", "execparam"), modes=("readwrite", "readonly"), includeNil=True))
-        expectedProps.extend(self.getPropertySet(kinds=("allocate",), action="external", includeNil=True))
-        props = self.comp.query([])
-        props = dict((x.id, any.from_any(x.value)) for x in props)
-        # Query may return more than expected, but not less
-        for expectedProp in expectedProps:
-            self.assertEquals(props.has_key(expectedProp.id), True)
-        
-        #######################################################################
-        # Verify that all expected ports are available
-        for port in self.scd.get_componentfeatures().get_ports().get_uses():
-            port_obj = self.comp.getPort(str(port.get_usesname()))
-            self.assertNotEqual(port_obj, None)
-            self.assertEqual(port_obj._non_existent(), False)
-            self.assertEqual(port_obj._is_a("IDL:CF/Port:1.0"),  True)
-            
-        for port in self.scd.get_componentfeatures().get_ports().get_provides():
-            port_obj = self.comp.getPort(str(port.get_providesname()))
-            self.assertNotEqual(port_obj, None)
-            self.assertEqual(port_obj._non_existent(), False)
-            self.assertEqual(port_obj._is_a(port.get_repid()),  True)
-            
-        #######################################################################
-        # Make sure start and stop can be called without throwing exceptions
-        self.comp.start()
-        self.comp.stop()
-        
-        #######################################################################
-        # Simulate regular component shutdown
-        self.comp.releaseObject()
-        
-    # TODO Add additional tests here
-    #
-    # See:
-    #   ossie.utils.bulkio.bulkio_helpers,
-    #   ossie.utils.bluefile.bluefile_helpers
-    # for modules that will assist with testing components with BULKIO ports
+print '*****************************************'
+print '******* Liquid FM Demod Unit Test *******'
+print '*****************************************'
+
+
+print '\n*********** Generating Data *************'
+fs = 256000.0 		# Sample Rate
+fm = 250.0 			# Signal Frequency (Hz)
+fc = 1000.0 		# Carrier Frequency (Hz)
+b = (fc - fm) / fm	# Modulation Index
+
+# Generate a time signal
+t = np.arange(0, 1, 1.0 / fs)
+
+# Generate the message signals for modulation
+x = np.sin(2 * np.pi * fm * t)
+
+#Modulate the signal
+modfm = np.cos(2 * np.pi * fc * t + b * x)
+
+#Apply the Hilbert Transform
+complexModFM = sp.hilbert(modfm)
+
+#Data to be pushed
+pushComplexModFM = np.ndarray(2 * len(complexModFM))
+for i in range(0,len(pushComplexModFM),2):
+	pushComplexModFM[i] = complexModFM[i/2].real
+	pushComplexModFM[i+1] = complexModFM[i/2].imag
+
+print '\n********* Creating Components ***********'
+#Set up components and helpers for the waveform
+fmDemod = sb.launch('../FMDemodulator.spd.xml', execparams={'DEBUG_LEVEL':5})
+dataSource = sb.DataSource(bytesPerPush=pushComplexModFM.nbytes)
+dataSink = sb.DataSink()
+
+print '\n********* Creating Connections **********'
+#Make connections
+dataSource.connect(fmDemod)
+fmDemod.connect(dataSink)
+print 'Connections Created'
+
+#Start sandbox env
+print '\n*********** Starting Sandbox ************'
+sb.start()
+print 'Components Started'
+
+#Run unit test
+print 'Pushing data'
+dataSource.push(pushComplexModFM.tolist(), sampleRate=fs, complexData=False)
+time.sleep(1)
+output = dataSink.getData()
+
+#Stop sandbox env
+print '\n*********** Stopping Sandbox ************'
+sb.stop()
+print 'Components stopped'
+
+#Index begins when data is no longer skewed
+index = 100000
+while abs(output[index]) > 0.001:
+	index += 1
+
+#Set values up to index to 0 for more accurate results
+for i in range(0, index):
+	output[i] = 0
+
+# Remove DC Bias from output
+bias = sum(output)/float(len(output))
+output = [(item-bias) for item in output]
+
+# Scale output to have same magnitude as input
+output_max = max(output[index:len(x)])
+x_max = max(x[index:len(x)])
+gain_adjust = output_max/x_max
+output = [(item/gain_adjust) for item in output]
+
+# Shift output to have same phase as input
+count = 0
+output_i = 0
+if output[index] > 0:
+	last_sign = 1
+else:
+	last_sign = -1
+
+for i,j in enumerate(output[index:]):
+	if j > 0:
+		sign = 1
+	else:
+		sign = -1
+
+	if last_sign != sign:
+		count += 1
+		last_sign = sign
+
+	if count == 10:
+		output_i = i
+
+count = 0
+x_i = 0
+if x[index] > 0:
+	last_sign = 1
+else:
+	last_sign = -1
+
+for i, j in enumerate(x[index:]):
+	if j > 0:
+		sign = 1
+	else:
+		sign = -1
+
+	if last_sign != sign:
+		count += 1
+		last_sign = sign
+	if count == 10:
+		x_i = i
+
+difference = x_i - output_i
+output = np.roll(output, difference)
+
+# Plot the data
+#plt.plot(x)
+#plt.plot(modfm)
+#plt.plot(output)
+#plt.show()
+
+#Validate results
+assert len(x) == len(output), 'Input signal and output result are not equal'
+sumError = sum([abs(y-z) for y,z in zip(output[index:],x[index:])])
+meanError = sumError/len(output)
+#print 'meanError =',meanError
+passed = True
+if meanError > .1:
+	passed = False
+                
+print '\n************** Results ****************'
+if passed:
+    print "Unit Test 1 .........................",u'\u2714'
+else:
+    print "Unit Test 1 .........................",u'\u2718'
     
-if __name__ == "__main__":
-    ossie.utils.testing.main("../FMDemodulator.spd.xml") # By default tests all implementations
+print '\nTest Complete\n'
